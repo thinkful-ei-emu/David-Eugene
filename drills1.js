9aa2920438a1828703799294979aab486f169e05
geneggy <geneggy@gmail.com>
2019-06-05 08:53:59 -0700

    completed object updater

--- drills1.js
+++ drills1.js
@@ -11,3 +11,12 @@ function createMyObject(){
   };
 }
 console.log(createMyObject());   
+
+function updateObject(obj) {
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz ='bizz';
+  obj.bang = 'bang';
+  return obj;
+}
+
