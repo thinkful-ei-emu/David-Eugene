aefbf32dd8be4f6d41394616a7afc02247150e2d
David Queen <dsqjr@msn.com>
2019-06-05 11:48:11 -0400

    added objcreator drill

--- drills1.js
+++ drills1.js
@@ -1,1 +1,13 @@
+/* eslint-disable no-console */
 //object drills
+'use strict';
+function createMyObject(){
+
+  return {
+    foo: 'bar',
+    answersToUniverse:42,
+    'olly olly':'oxen free',
+    sayHello:()=> {return 'hello';}
+  };
+}
+console.log(createMyObject());   
