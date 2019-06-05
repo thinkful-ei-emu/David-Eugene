1ce1093b706d31681f5e3d9643847b0498c5e7e9
geneggy <geneggy@gmail.com>
2019-06-05 09:02:57 -0700

    completed final drill

--- drills1.js
+++ drills1.js
@@ -35,3 +35,17 @@ function personMaker() {
 }
 console.log(personMaker());
 
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+
+console.log(keyDeleter(sampleObj));
