296bf91b72a2cb71972de1a289d3eeafe010861e
David Queen <dsqjr@msn.com>
2019-06-05 14:15:24 -0400

    did drills 3 and 4

--- drills3.js
+++ drills3.js
@@ -22,3 +22,20 @@ const fooObject = {
 for(let key in fooObject) {
   console.log(`${key} : ${fooObject[key]}`);
 }
+///
+const menu ={
+  meals:['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+};
+console.log(menu.meals[3]);
+
+class person{
+  constructor(Name, JobTitle){
+    this.name=Name;
+    this.jobTitle = JobTitle;
+  }
+}
+let people=[new person('David','Programmer'), new person('eugene','auditor'),new person('Devin','sales Rep')];
+
+for(let persons in people){
+  console.log(people[persons]);
+}
